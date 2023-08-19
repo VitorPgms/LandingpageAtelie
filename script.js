68230901eceedc43132cbb71ce2c65b6f8ae026d
Vitor de Paula Gomes <112875256+VitorPgms@users.noreply.github.com>
2023-08-18 23:09:58 -0300

    att

---
+++ script.js
@@ -0,0 +1,15 @@
+window.sr = ScrollReveal({reset: true})
+
+sr.reveal('.home-content', {duration: 2000});
+
+sr.reveal('.img-home', {duration: 2000});
+
+sr.reveal('.style', {
+    rotate: {x:0, y:80, z:0},
+    duration: 2000
+});
+
+sr.reveal('.btn', {
+    rotate: {x:0, y:80, z:0},
+    duration: 3000
+});
